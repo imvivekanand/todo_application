4660e58408fe3cbf0d3101c8842db5dcbdfdbcbe
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-03-26 22:52:16 +0530

    Project Initialized

---
+++ todo_app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+import Input from "./components/input";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="bg-black h-screen p-3">
+      <div className="mx-auto max-w-[750px] min-h-[550px] shadow-2xl bg-slate-50 rounded-xl">
+        <Input />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo_app/src/components/Input.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Input = () => {
+  return (
+    <div>
+    <input type="text"/>
+    <div>
+      +
+    </div>
+    </div>
+  )
+}
+
+export default Input
